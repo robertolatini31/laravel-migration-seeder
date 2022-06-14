dfdf4cd5501e1240c49f307dc1a4433dc78c882b
robertolatini31 <robertolatini31@gmail.com>
2022-06-14 14:49:27 +0200

    add model and migration

---
+++ database/migrations/2022_06_14_123313_create_trips_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('price', 6, 2)->unsigned();
+            $table->tinyInteger('duration');
+            $table->string('place', 255);
+            $table->text('description')->nullable();
+            $table->string('image', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
